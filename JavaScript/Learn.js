f8bc152bf3202dd37efa0f7c46aac21b30b1310c
SamyamRimal <samyamrimal@gmail.com>
2025-11-20 23:25:09 +0545

    Done for the day.

--- JavaScript/Learn.js
+++ JavaScript/Learn.js
@@ -1,1 +1,25 @@
 console.log("Samyam Rimal");
+document.write("Samyam");
+
+let a = 10;
+let b = 25;
+console.log("Sum of " +a+ " and " +b+ " is:"+(a+b)); //This is like java way
+console.log(`Sum of ${a} and ${b} is: ${a+b}`); // This is JavaScript way
+
+//Arthematic Operators
+console.log(a+b);
+console.log(a-b);
+console.log(a/b);
+console.log(a*b);
+console.log(a**b);
+
+//Uinary Operators
+console.log(++a); // first Increases
+console.log(a++); // first prints value then increases
+
+//Assignment Operators
+a+=5; // This means a=a+5; in shoet form. We can use (+,*,-,/,%).
+
+// Comparison Operator 
+a>b;
+
