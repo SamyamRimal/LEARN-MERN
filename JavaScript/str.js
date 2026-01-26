8c415f6e438369e6b679b91467c5007693f8f202
SamyamRimal <samyamrimal@gmail.com>
2026-01-26 12:35:46 +0545

    Strings in JavaScript

---
+++ JavaScript/str.js
@@ -0,0 +1,13 @@
+let msg = "hello";
+
+console.log(msg.trim().toUpperCase()); // Using Multiple Methods
+
+let name = "ApnaCollege";
+
+console.log(name.slice(4,11)); // String Method with Arguents
+
+console.log(name.replace("Apna","Our"));
+
+console.log(name.indexOf("na"));
+
+console.log(name.repeat(2));
