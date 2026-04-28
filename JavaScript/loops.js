4fa083ea6b28f1c21602c9e6096911e745a71218
SamyamRimal <samyamrimal@gmail.com>
2026-04-28 19:28:36 +0545

    Multiplication Table using loop

--- JavaScript/loops.js
+++ JavaScript/loops.js
@@ -5,17 +5,33 @@
 // }
 
 
-//Print even numbers from (1 to 15)
-console.log("Even")
-for(let i=1; i<=15; i++){
-    if(i%2==0){
-        console.log(i);
-    }
-}
-console.log("Odd");
-//Print odd numbers from (1 to 15)
-for(let i=1; i<=15; i++){
-    if(i%2!=0){
-        console.log(i);
-    }
+// //Print even numbers from (1 to 15)
+// console.log("Even")
+// for(let i=1; i<=15; i++){
+//     if(i%2==0){
+//         console.log(i);
+//     }
+// }
+// console.log("Odd");
+// //Print odd numbers from (1 to 15)
+// for(let i=1; i<=15; i++){
+//     if(i%2!=0){
+//         console.log(i);
+//     }
+// }
+
+//Multiplication of 5
+// console.log("Multiplication of 5");
+
+// for(let i=1; i<=10; i++){
+//     let m=5*i;
+//     console.log("5 x "+i+" = " +m);
+// }
+
+//Multiplication of User Choice
+let n = prompt("Enter a number");
+console.log("Multiplication of "+n)
+for (let i=1; i<=10; i++){
+    let m=n*i;
+    console.log(n+" x "+i+" = "+m);
 }
