d9ac172400df16a04d515e20a73d89eb84bfe056
SamyamRimal <samyamrimal@gmail.com>
2026-04-28 19:09:01 +0545

    Odd or Even using loop

---
+++ JavaScript/loops.js
@@ -0,0 +1,21 @@
+//let n=prompt("Enter a number");
+
+// for(let i=1;i<=n;i++){
+//     console.log("Samyam Rimal"+i);
+// }
+
+
+//Print even numbers from (1 to 15)
+console.log("Even")
+for(let i=1; i<=15; i++){
+    if(i%2==0){
+        console.log(i);
+    }
+}
+console.log("Odd");
+//Print odd numbers from (1 to 15)
+for(let i=1; i<=15; i++){
+    if(i%2!=0){
+        console.log(i);
+    }
+}
