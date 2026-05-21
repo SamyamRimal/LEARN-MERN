fd3ada0d76466e3e14a79f148f31c99df1090032
SamyamRimal <samyamrimal@gmail.com>
2026-05-21 17:14:52 +0545

    Guessing Game

---
+++ JavaScript/Prct.js
@@ -0,0 +1,26 @@
+// Generate a random number between 1 and 100.
+// let num= Math.random()*100+1;
+// let result = Math.floor(num)
+
+
+//Guess the Random number
+
+const max = prompt ("Enter the Max range of numbers.");
+
+const random = Math.floor(Math.random()*max+1);
+
+let guess = prompt ("Guess your Number");
+while(true){
+    if(guess == "quit"){
+        console.log("You Quited");
+        break;
+    }
+    if (guess == random){
+        console.log("You guesses the correct number! which is", random);
+        break;
+    }
+    else{
+        guess = prompt("Incorrect Guess!, Try again");
+    }
+}
+
